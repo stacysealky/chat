e885c2a9f1e3c6e388766388002f0c72e0c2bd02
Stacy Sealky <stacy.sealky.lee@gmail.com>
2020-08-04 19:39:25 -0500

    fixed the db issue with saving and loading

--- public_html/client.js
+++ public_html/client.js
@@ -9,37 +9,85 @@
  * then begin to send messages using the other text field and the “Send Messages” button.
  */
 
+$(document).ready(function () {
+  //SUBMIT FORM
+  $("#msgForm").submit(function (event) {
+    event.preventDefault();
+    ajaxPost();
+  });
+
+  getMsg();
+
+  function ajaxPost() {
+    let alias = $('#alias').val();
+    let msg = $('#message').val();
+    let itemObj = {
+      alias: alias,
+      message: msg
+    };
+    $.ajax({
+      type: "POST",
+      contentType: "application/json",
+      url: '/chats/post/',
+      data: JSON.stringify(itemObj),
+      dataType: 'json',
+      success: function (result) {
+        alert('message sent!');
+        console.log(result);
+      },
+      error: function (e) {
+        console.log("ERROR: ", e);
+      }
+    });
+    reset();
+  }
+
+  function reset() {
+    $('#alias').val("");
+    $('#message').val("");
+  }
+
+  //adding messages to the #content div
+  function postMsg(m) {
+    $("#dataContent").append(
+      `<p><span>${m.alias}:</span>  ${m.message}</p>`
+    )
+  };
+
+  //calling the get request to get data from the server
+  function getMsg() {
+    $.get('/chats', (data) => {
+      data.forEach(postMsg);
+    });
+  }
+
+  
+});
+
+//refreshs the page
+function refresh() {
+  setTimeout(function () {
+    location.reload();
+  }, 100);
+}
+
 //setting up socket io client side
-var socket = io.connect('http://localhost:3000');
-socket.on('message', postMsg);
+// var socket = io.connect('http://localhost:3000');
+// socket.on('message', postMsg);
 
 // This submits the alias and the message to the server
 // once the user clicks the submit button
-$(() => {
-  $("#submit").click(() => {
-    submitMsg({
-      alias: $("#alias").val(),
-      message: $("#message").val()
-    });
-  })
-  getMsg();
-})
-
-//adding messages to the #content div
-function postMsg(message) {
-  $("#content").append(
-    `<p>${message.alias}:  ${message.message}</p>`
-  )
-}
-
-//calling the get request to get data from the server
-function getMsg() {
-  $.get('http://localhost:3000/messages', (data) => {
-    data.forEach(postMsg);
-  });
-}
+// $(() => {
+//   $("#submit").click(() => {
+//     submitMsg({
+//       alias: $("#alias").val(),
+//       message: $("#message").val()
+//     });
+//   })
+//   getMsg();
+// })
 
 //posting each message
-function submitMsg(message) {
-  $.post('http://localhost:3000/messages', message);
-}
+// function submitMsg(message) {
+//   $.post('http://localhost:3000/chats/post', message);
+// }
--- server.js
+++ server.js
@@ -20,61 +20,118 @@ const mongoose = require('mongoose');
 const parser = require('body-parser');
 const app = express();
 const path = require('path');
-const port = 3000;
 const http = require('http');
 var server = http.createServer(app);
+const ejs = require('ejs');
+
 const io = require('socket.io').listen(server);
 
 app.use(express.static(path.join(__dirname, 'public_html')));
+
+//Set view engine to ejs
+app.set("view engine", "ejs");
+app.engine('html', ejs.renderFile);
+
+
 app.use(parser.json());
 app.use(parser.urlencoded({
   extended: true
 }));
 
-const db = mongoose.connection;
-const mongoDBURL = 'mongodb://127.0.0.1/auto';
+//DEV ENV
+const hostname = '127.0.0.1';
+const port = 3000;
 
-//default mongoose connection
-mongoose.connect(mongoDBURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// PRODUCTION ENV 
+// const hostname = '159.65.223.7';
+// const port = 443;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected
+//DB URI - Move to config.env if pushing code
+var uri = 'mongodb+srv://stacysealky:pwd123@clusterstacy.fco5r.mongodb.net/chatty';
+
+//DB SET UP & APP LISTEN
+var options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+};
+mongoose.connect(uri, options, (err, database) => {
+	if (err) {
+		console.log('Unable to connect to DB. Error:', err);
+	} else {
+		console.log('Connected to Mongodb');
+		app.listen(port, hostname, () => {
+			console.log(`Server running at http://${hostname}:${port}/`);
+		});
+	}
 });
 
 //setup schema
 var Schema = mongoose.Schema;
+
 var MsgSchema = new Schema({
   alias: String,
   message: String
+}, {
+	collection: 'messages'
 });
 
 var Messages = mongoose.model('Messages', MsgSchema);
 
-
+//Decided not to use socket io
 //connecting socket io
-io.on('connection', () => {
-  console.log("user is connected");
-});
-
-
-server.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// io.on('connection', () => {
+//   console.log("user is connected");
+// });
 
 //routing for post
-app.post('/messages', (req, res) => {
-  var message = new Messages(req.body);
-  message.save();
-  res.sendStatus(200);
+app.post('/chats/post/', async (req, res) => {
+  try {
+    var m = new Messages();
+    m.alias = req.body.alias;
+    m.message = req.body.message;
+    m.save((err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("successfully saved");
+      }
+    });
+    res.status(200).redirect('/chats');
+  } catch (err) {
+    res.status(400).send(JSON.stringify({
+			success: false,
+			message: err
+		}));
+  }
+  // var message = new Messages(req.body);
+  // message.save();
+  // res.sendStatus(200);
 });
 
 //routing for get
-app.get('/messages', (req, res) => {
-  var m = mongoose.model('Messages', MsgSchema);
-  m.find({}, (err, messages) => {
-    // console.log(messages)
-    res.send(messages);
-  })
+app.get('/chats', async (req, res) => {
+  try {
+    const find = await Messages.find({}, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+        res.send(data);
+      }
+    });
+	} catch (error) {
+		res.status(400).send(JSON.stringify({
+			success: false
+		}));
+	}
+  // Messages.find({}, (err, messages) => {
+  //   // console.log(messages)
+  //   res.send(messages);
+  // })
+});
+
+//UNHANDLED REJECTION - Promise
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+	// server.close(() => process.exit(1));
 })
